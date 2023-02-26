4b9b2b20966652374fb97a3269815db3e6f03b56
Asım Ates <asmates89@gmail.com>
2023-02-26 15:56:39 +0300

    a

--- soruCozumu_01.sql
+++ soruCozumu_01.sql
@@ -1,11 +1,17 @@
 /*
-SORU ÇÖZÜMÜ - 1 :asdsad
+SORU ÇÖZÜMÜ - 1 :
 -> film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 -> film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60'dan büyük ve 75'ten küçük olma koşullarıyla sıralayınız.
 -> film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 ve replacement_cost 12.99 veya 28.99 olma koşullarıyla sıralayınız.
 -> customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değerini sıralayınız.
 -> film tablosundaki uzunluğu (length) 50'den büyük olmayıp aynı zamanda rental_rate değeri 2.99 veya 4.99 olmayan verileri sıralayınız.
 */
+
+select *
+from film;
+
+select * from
+customer;
 
 --film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 select title, description
