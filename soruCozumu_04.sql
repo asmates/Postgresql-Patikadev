bb2920ced000d7aa66bb624ce21d3f48c86b4635
Asım Ates <asmates89@gmail.com>
2023-03-10 22:48:55 +0300

    10/03/2023

--- soruCozumu_04.sql
+++ soruCozumu_04.sql
@@ -2,9 +2,9 @@
 SORU ÇÖZÜMÜ - 4 :
 -> film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
 -> film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
--> film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+-> film tablosunda bulunan film isimlerinden (title) kaç tanesi 'T' karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 -> country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
--> city tablosundaki şehir isimlerinin (title) kaç tanesi 'R' veya r karakteri ile biter?
+-> city tablosundaki şehir isimlerinin (title) kaç tanesi 'R' veya 'r' karakteri ile biter?
 */
 
 select *
@@ -24,7 +24,7 @@ from film;
 select count (distinct (replacement_cost))
 from film;
 
---film tablosunda bulunan film isimlerinde kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+--film tablosunda bulunan film isimlerinden (title) kaç tanesi 'T' karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 select count (title) 
 from film
 where title like 'T%' and rating = 'G';
@@ -34,7 +34,7 @@ select count (country)
 from country
 where country like '_____';
 
---city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+-- city tablosundaki şehir isimlerinin (title) kaç tanesi 'R' veya 'r' karakteri ile biter?
 select count (city)
 from city
 where city ilike '%r';
