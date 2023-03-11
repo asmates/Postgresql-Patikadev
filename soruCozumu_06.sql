f4baa07c774f75cc1144fc1421fae04d6b2cb939
Asım Ates <asmates89@gmail.com>
2023-03-11 22:51:12 +0300

    11/03/2023

--- soruCozumu_06.sql
+++ soruCozumu_06.sql
@@ -2,8 +2,8 @@
 SORU ÇÖZÜMÜ - 6 :                                                                                                                                                                                  
 -> film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
 -> film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
--> film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
--> film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+-> film tablosunda bulunan filmlerden rental_rate değeri 0.99'a eşit olan en uzun (length) film kaç dakikadır?
+-> film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanların kaç farklı replacement_cost değeri vardır?
 */
 
 select *
@@ -18,12 +18,12 @@ select count (title) as film_sayisi
 from film
 where title like 'C%';
 
---film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
-select max(length) as enuzun_film
+--film tablosunda bulunan filmlerden rental_rate değeri 0.99'a eşit olan en uzun (length) film kaç dakikadır?
+select max (length) as enuzun_film
 from film
 where rental_rate = 0.99;
 
---film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+--film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanların kaç farklı replacement_cost değeri vardır?
 select count (distinct (replacement_cost))
 from film
 where length > 150;
