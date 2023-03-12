61e95f4b32e2fb5c58ba12089a8517b6df630d39
Asım Ates <asmates89@gmail.com>
2023-03-12 20:06:20 +0300

    12/03/2023

--- soruCozumu_07.sql
+++ soruCozumu_07.sql
@@ -1,11 +1,11 @@
 /*
 SORU ÇÖZÜMÜ - 7 :                                                                                                                                                                                  
 -> film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
--> film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50'den fazla olan
+-> film tablosunda bulunan filmleri replacement_cost sütununa göre gruplandığında film sayısı 50'den fazla olan
    replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
--> customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+-> customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayıları nelerdir?
 -> city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran
-   country_id bilgisini ve şehir sayısını paylaşınız.
+   country_id bilgisini ve şehir sayısını sıralayınız.
 */
 
 select *
@@ -22,20 +22,20 @@ select rating
 from film
 group by rating;
 
---film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50'den fazla olan
+--film tablosunda bulunan filmleri replacement_cost sütununa göre gruplandığında film sayısı 50'den fazla olan
 --replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
 select replacement_cost, count (title) as filmsayisi
 from film
 group by replacement_cost
 having count (title) > 50;
 
---customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayıları nelerdir?
 select store_id, count (*) as musteri_sayisi
 from customer
 group by store_id;
 
 --city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran
---country_id bilgisini ve şehir sayısını paylaşınız.
+--country_id bilgisini ve şehir sayısını sıralayınız.
 select country_id, count (city) as sehirsayisi
 from city
 group by country_id
